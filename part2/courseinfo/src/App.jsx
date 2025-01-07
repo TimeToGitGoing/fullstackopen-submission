ab1eaac2c1bbdf649a8ccbe3100eb709a6804379
Huda <iamanotherday@gmail.com>
2025-01-07 08:25:57 +0200

    Deleted unnecessary lines

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ name }) => <h1>{name}</h1>
 
 const Total = ({ sum }) => <p><b>Total of {sum} exercises</b></p>
 
@@ -7,24 +7,10 @@ const Part = ({ part }) =>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
-
 const Course = ({ course, sum }) => {
-  console.log(sum)
   return (
     <>
-      <Header course={course.name}/>
+      <Header name={course.name}/>
       <Part part={course.parts[0]}/>
       <Part part={course.parts[1]}/>
       <Part part={course.parts[2]}/>
